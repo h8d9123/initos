27f42e8e3698bd8f0d91097d7db0c389d907e025
kirito <2466621355@qq.com>
2019-05-03 06:52:55 +0800

    config-vim

--- vimplus-config.sh
+++ vimplus-config.sh
@@ -1,3 +1,14 @@
-git clone https://github.com/chxuan/vimplus.git ~/.vimplus
-cd ~/.vimplus
-sodo ./install.sh
+# intall all
+
+#git clone https://github.com/chxuan/vimplus.git ~/.vimplus
+#cd ~/.vimplus
+#sodo ./install.sh
+
+
+#only plugin
+git clone git@github.com:h8d9123/initos.git
+echo "mv vim files to ~/vimbak"
+mkdir ~/vimbak
+mv ~/.vim* ~/vimbak
+tar zxvf vim-config.tgz -C ~
+echo "finished to config vim"
